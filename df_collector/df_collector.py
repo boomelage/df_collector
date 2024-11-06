e3b1fbde525aee66796adec4fa9d039a17fa1f55
radusbriciu <radusbriciu@proton.me>
2024-11-06 13:49:38 +0000

    improvements

--- df_collector/df_collector.py
+++ df_collector/df_collector.py
@@ -14,6 +14,7 @@ class df_collector:
 		files = [f for f in os.listdir(datadir) if f.endswith('.csv')]
 		files = [os.path.join(datadir, f) for f in files]
 		dfs = Parallel(n_jobs=n_jobs)(delayed(self.collect_df)(f) for f in files)
+		dfs = [df for df in dfs if not df.empty]
 		if len(dfs)>0:
 			return pd.concat(dfs,ignore_index=True)
 		else:
